05f9274230f5703b1d42c571890c0928682cebad
unknown <23114290018@ACADEMICO.IESB.BR>
2025-03-06 21:39:16 -0300

    subindo

---
+++ aula02.js
@@ -0,0 +1,74 @@
+//atividade 1
+let teste = "Anninha"
+let maiuscula = teste.toUpperCase()
+let minuscula = teste.toLowerCase()
+let converter = teste.split("").reverse().join("")
+let substituir = teste.replace("Anninha","Rezende")
+
+console.log(maiuscula)
+console.log(minuscula)
+console.log(converter)
+console.log(substituir)
+
+
+//atividade 2
+
+let somar = 15 + 5
+let subtrair = 15 - 5
+let multiplicar = 15 * 5
+let dividir = 15 / 5
+let maiorQue = 15 > 5
+
+console.log(somar)
+console.log(subtrair)
+console.log(multiplicar)
+console.log(dividir)
+console.log(maiorQue)
+
+
+//atividade 3
+
+idade = 70
+
+
+if (idade >= 18) {
+  console.log("Maior de idade")
+} else if(idade >= 65) {
+  console.log("Idosa")
+} else {
+  console.log("Menor de idade")
+}
+
+//atividade 4
+
+for(let i = 0; i < 101; i++){
+    console.log(i)
+}
+
+//atividade 5
+
+
+
+//atividade 6
+
+const numeros = [10,20,30,40,50]
+
+numeros.push(60)
+console.log(numeros)
+numeros.shift()
+console.log(numeros)
+let maior = Math.max(...numeros)
+console.log(maior)
+let menor = Math.min(...numeros)
+console.log(menor)
+
+//atividade 7
+
+
+
+//atividade 8
+
+
+
+//atividade 9
+//atividade 10
